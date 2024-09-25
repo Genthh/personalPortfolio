1a013b4655e9977cbc71f6875eb13b9f80cac718
Genthh <101422592+Genthh@users.noreply.github.com>
2024-09-25 15:16:00 +0200

    fixing bugs

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -1,15 +1,53 @@
 "use client";
 import About from "@/components/About";
+import { Loader } from "@/components/Loader";
 import { Navbar } from "@/components/Navbar";
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useEffect, useRef, useState } from "react";
 
 const page = () => {
+  const [loading, setLoading] = useState(true);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      gsap.to(containerRef.current, {
+        opacity: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        onComplete: () => {
+          setLoading(false);
+        },
+      });
+    }, 100);
+  }, []);
+
+  useGSAP(() => {
+    if (!loading) {
+      gsap.fromTo(
+        containerRef.current,
+        { opacity: 0 },
+        { opacity: 1, duration: 1, ease: "power2.out" }
+      );
+    }
+  }, [loading]);
   return (
-    <div className="max-w-6xl h-dvh flex  flex-col   md:mx-auto  md:px-4 mx-5 ">
-      <Navbar />
-      <div className=" md:mx-28 ">
-        <About />
-      </div>
+    <div
+      ref={containerRef}
+      className="max-w-6xl h-dvh flex  flex-col   md:mx-auto  md:px-4 mx-5 "
+    >
+      {loading ? (
+        <></>
+      ) : (
+        // <Loader />
+        <>
+          <Navbar />
+          <div className=" md:mx-28 ">
+            <About />
+          </div>
+        </>
+      )}
     </div>
   );
 };
