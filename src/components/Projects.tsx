8af085e744aa389a1cf738411ec1286a362ddd6a
Pirroxx <101422592+Genthh@users.noreply.github.com>
2024-09-28 18:35:30 +0200

    adding more projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,12 +11,21 @@ import Dwello from "../../public/images/dwello.png";
 import GentPortfolio from "../../public/images/gentportfolio.png";
 import ChuckNorris from "../../public/images/chuckNorris.png";
 import Thisproject from "../../public/images/thisproject.png";
+import innovemia from "../../public/images/innovemia.png";
+import Metdaan from "../../public/images/metdaan.png";
+import Pabau from "../../public/images/pabau.png";
 
 const slides = [
   {
-    id: 2,
+    id: 0,
     title: "This Project",
-    technologies: ["Next Js", "Gsap", "Tailwind CSS"],
+    technologies: [
+      "Next Js",
+      "TypeScript",
+      "Gsap",
+      "Tailwind CSS",
+      "Express Js",
+    ],
     content:
       "This personal portfolio website showcases my skills, projects, and experiences. Built with Next.js, TypeScript, GSAP for animations, and Tailwind CSS for styling, it is designed to be responsive and visually appealing, providing a great user experience on both desktop and mobile devices.",
     image: Thisproject,
@@ -24,17 +33,25 @@ const slides = [
     deploymentLink: "https://pirrox.dev",
   },
   {
-    id: 2,
-    title: "Dwello Project",
-    technologies: ["Next Js", "Gsap", "Tailwind CSS"],
+    id: 1,
+    title: "Pabau",
+    technologies: [
+      "Next Js",
+      "TypeScript",
+      "Less",
+      "GraphQL",
+      "Hasura",
+      "Prisma",
+    ],
     content:
-      "The Dwello app is designed to help users find their perfect home with an interactive and visually appealing interface. The application leverages the power of Next.js for server-side rendering and GSAP for smooth and engaging animations.",
-    image: Dwello,
-    githubLink: "https://github.com/Genthh/dwello",
-    deploymentLink: "https://dwello-g5mg.vercel.app/",
+      "Pabau is a robust platform built with Next.js, utilizing LESS for styling, and leveraging GraphQL with Hasura and Prisma for efficient data management. I was part of the team working on this project, contributing to its development and performance optimization.",
+    image: Pabau,
+    githubLink: "https://github.com/Genthh/Chuck-Norris-jokes",
+    deploymentLink: "https://pabau.com/",
   },
+
   {
-    id: 3,
+    id: 2,
     title: "Gent Portfolio V1",
     technologies: ["Next Js", "Three js", "Gsap", "Tailwind CSS"],
     content:
@@ -44,14 +61,44 @@ const slides = [
     deploymentLink: "https://gent-hulaj.vercel.app/",
   },
   {
-    id: 4,
+    id: 3,
     title: "Chuck Norris Jokes",
-    technologies: ["Next Js", "ThreeGsap", "Tailwind CSS"],
+    technologies: ["Next Js", "Tailwind CSS"],
     content:
       "Chuck Norris Jokes is a fun and interactive web application that fetches and displays random Chuck Norris jokes. Built with Next.js for performance. Tailwind CSS ensures a responsive and clean user interface.",
     image: ChuckNorris,
     githubLink: "https://github.com/Genthh/Chuck-Norris-jokes",
     deploymentLink: "https://chuck-norris-jokes-blue.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Innovemia Academy",
+    technologies: ["Next Js", "i18n", "Sass"],
+    content:
+      "Innovemia Academy is a versatile platform built with Next.js, offering seamless multilingual support through i18n and featuring a sleek, responsive design powered by Sass. It delivers a user-friendly and engaging experience, tailored for a wide audience, ensuring accessibility and smooth navigation across devices.",
+    image: innovemia,
+    githubLink: "https://github.com/Genthh/Chuck-Norris-jokes",
+    deploymentLink: "https://innovemia.academy/",
+  },
+  {
+    id: 5,
+    title: "MetDaan",
+    technologies: ["Next Js", "TypeScript", "Sass"],
+    content:
+      "MetDaan is a modern platform built with Next.js for fast performance and seamless navigation. Styled with Sass, it offers a sleek, responsive design that adapts across devices. By combining advanced technology with intuitive design, MetDaan ensures an engaging and high-quality user experience.",
+    image: Metdaan,
+    githubLink: "https://github.com/Genthh/Chuck-Norris-jokes",
+    deploymentLink: "https://metdaan.media/",
+  },
+  {
+    id: 6,
+    title: "Dwello Project",
+    technologies: ["Next Js", "TypeScript", "Gsap", "Tailwind CSS"],
+    content:
+      "The Dwello app is designed to help users find their perfect home with an interactive and visually appealing interface. The application leverages the power of Next.js for server-side rendering and GSAP for smooth and engaging animations.",
+    image: Dwello,
+    githubLink: "https://github.com/Genthh/dwello",
+    deploymentLink: "https://dwello-g5mg.vercel.app/",
   },
 ];
 
@@ -99,7 +146,7 @@ const Slider: React.FC<SliderProps> = () => {
                 <h1 className="text-2xl font-bold text-center text-darkPurple">
                   {slide.title}
                 </h1>
-                <div className="flex flex-wrap mt-1 gap-x-2 ">
+                <div className="flex flex-wrap mt-1 gap-2 ">
                   {slide.technologies.map((tech, index) => (
                     <button
                       key={index}
@@ -113,8 +160,8 @@ const Slider: React.FC<SliderProps> = () => {
               <div className="flex flex-wrap">
                 <p className="">{slide.content}</p>
               </div>
-              <div className="flex gap-x-3 h-32  items-end">
-                <div className="md:w-1/2   ">
+              <div className="flex gap-x-3 h-32 items-end">
+                <div className="md:w-1/2">
                   <Image
                     src={slide.image}
                     alt={slide.title}
@@ -123,15 +170,18 @@ const Slider: React.FC<SliderProps> = () => {
                     className="rounded shadow-2xl max-w-full"
                   />
                 </div>
-                <a
-                  href={slide.githubLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Tooltip title="GitHub Repository" placement="top" arrow>
-                    <GitHub sx={{ fontSize: 35, cursor: "pointer" }} />
-                  </Tooltip>
-                </a>
+
+                {![1, 4, 5].includes(slide.id) && (
+                  <a
+                    href={slide.githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Tooltip title="GitHub Repository" placement="top" arrow>
+                      <GitHub sx={{ fontSize: 35, cursor: "pointer" }} />
+                    </Tooltip>
+                  </a>
+                )}
 
                 <a
                   href={slide.deploymentLink}
